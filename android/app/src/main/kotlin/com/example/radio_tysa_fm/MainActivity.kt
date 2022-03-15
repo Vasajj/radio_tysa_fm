f02cf07e9c8754c2d4ee125e5c77165e8a4bd31d
Vasajj <talapa.vasyl@gmail.com>
2022-03-15 12:56:32 +0200

    xiaomi 12android good

--- android/app/src/main/kotlin/com/example/radio_tysa_fm/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/radio_tysa_fm/MainActivity.kt
@@ -1,6 +1,18 @@
 package com.example.radio_tysa_fm
 
+//import io.flutter.embedding.android.FlutterActivity
+
+
+import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+
 }
+
