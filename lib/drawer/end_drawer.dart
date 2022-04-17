557fbfbdc9f8ab9cd0c65021a528e75068d7154f
Vasajj <talapa.vasyl@gmail.com>
2022-04-17 12:24:22 +0300

    endDrawer in newFolder

---
+++ lib/drawer/end_drawer.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+
+class EndDrawer extends StatelessWidget {
+  const EndDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.25,
+      child: Drawer(
+        backgroundColor: const Color(0xff00310b),
+        elevation: 20.0,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const CircleAvatar(
+              backgroundImage: AssetImage('assets/TysaAvatar.jpg'),
+              radius: 32,
+            ),
+            ListTile(
+              leading: IconButton(
+                icon: const Icon(
+                  FontAwesomeIcons.facebookF,
+                  color: Colors.white,
+                  size: 40,
+                ),
+                onPressed: () async {
+                  const url = 'https://www.facebook.com/suspilne.tysafm/';
+                  if (await canLaunch(url)) {
+                    await launch(url, forceSafariVC: false);
+                  } else {
+                    throw 'Could not launch $url';
+                  }
+                },
+              ),
+            ),
+            ListTile(
+              leading: IconButton(
+                onPressed: () async {
+                  const url = 'https://twitter.com/suspilne_tysafm';
+
+                  if (await canLaunch(url)) {
+                    await launch(url, forceSafariVC: false);
+                  } else {
+                    throw 'Could not launch $url';
+                  }
+                },
+                icon: const Icon(
+                  FontAwesomeIcons.twitter,
+                  color: Colors.white,
+                  size: 40,
+                ),
+              ),
+            ),
+            ListTile(
+                leading: IconButton(
+                  onPressed: () async {
+                    const url = 'https://www.instagram.com/suspilne.tysafm/';
+
+                    if (await canLaunch(url)) {
+                      await launch(url, forceSafariVC: false);
+                    } else {
+                      throw 'Could not launch $url';
+                    }
+                  },
+                  icon: const Icon(
+                    FontAwesomeIcons.instagram,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                )),
+            ListTile(
+                leading: IconButton(
+                  onPressed: () async {
+                    const url = 'http://t.me/suspilne_tysafm';
+
+                    if (await canLaunch(url)) {
+                      await launch(url, forceSafariVC: false);
+                    } else {
+                      throw 'Could not launch $url';
+                    }
+                  },
+                  icon: const Icon(FontAwesomeIcons.telegramPlane,
+                      color: Colors.white, size: 40),
+                )),
+            ListTile(
+              leading: IconButton(
+                onPressed: () async {
+                  const url = 'https://www.mixcloud.com/tysafm/';
+
+                  if (await canLaunch(url)) {
+                    await launch(url, forceSafariVC: false);
+                  } else {
+                    throw 'Could not launch $url';
+                  }
+                },
+                icon: const Icon(
+                  FontAwesomeIcons.mixcloud,
+                  color: Colors.white,
+                  size: 40,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: IconButton(
+                onPressed: () async {
+                  const url = 'http://twitch.tv/tysafm';
+
+                  if (await canLaunch(url)) {
+                    await launch(url, forceSafariVC: false);
+                  } else {
+                    throw 'Could not launch $url';
+                  }
+                },
+                icon: const Icon(Icons.connected_tv,
+                    color: Colors.white, size: 40),
+              ),
+            ),
+            ListTile(
+              leading: IconButton(
+                onPressed: () async {
+                  const url = 'tel:+38095 510 2277';
+
+                  if (await canLaunch(url)) {
+                    await launch(url, forceSafariVC: false);
+                  } else {
+                    throw 'Could not launch $url';
+                  }
+                },
+                icon: const Icon(Icons.phone_forwarded,
+                    color: Colors.white, size: 40),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:radio_player/radio_player.dart';
 import 'package:flutter/services.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tysa_fm/drawer/end_drawer.dart';
 
 void main() {
   runApp(
@@ -64,141 +64,9 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
     ]);
-
     return MaterialApp(
       home: Scaffold(
         resizeToAvoidBottomInset: false,
-        endDrawer: SizedBox(
-          width: MediaQuery.of(context).size.width * 0.25,
-          child: Drawer(
-            backgroundColor: const Color(0xff00310b),
-            elevation: 20.0,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                const CircleAvatar(
-                  backgroundImage: AssetImage('assets/TysaAvatar.jpg'),
-                  radius: 32,
-                ),
-                ListTile(
-                  leading: IconButton(
-                    icon: const Icon(
-                      FontAwesomeIcons.facebookF,
-                      color: Colors.white,
-                      size: 40,
-                    ),
-                    onPressed: () async {
-                      const url = 'https://www.facebook.com/suspilne.tysafm/';
-                      if (await canLaunch(url)) {
-                        await launch(url, forceSafariVC: false);
-                      } else {
-                        throw 'Could not launch $url';
-                      }
-                    },
-                  ),
-                ),
-                ListTile(
-                  leading: IconButton(
-                    onPressed: () async {
-                      const url = 'https://twitter.com/suspilne_tysafm';
-
-                      if (await canLaunch(url)) {
-                        await launch(url, forceSafariVC: false);
-                      } else {
-                        throw 'Could not launch $url';
-                      }
-                    },
-                    icon: const Icon(
-                      FontAwesomeIcons.twitter,
-                      color: Colors.white,
-                      size: 40,
-                    ),
-                  ),
-                ),
-                ListTile(
-                    leading: IconButton(
-                  onPressed: () async {
-                    const url = 'https://www.instagram.com/suspilne.tysafm/';
-
-                    if (await canLaunch(url)) {
-                      await launch(url, forceSafariVC: false);
-                    } else {
-                      throw 'Could not launch $url';
-                    }
-                  },
-                  icon: const Icon(
-                    FontAwesomeIcons.instagram,
-                    color: Colors.white,
-                    size: 40,
-                  ),
-                )),
-                ListTile(
-                    leading: IconButton(
-                  onPressed: () async {
-                    const url = 'http://t.me/suspilne_tysafm';
-
-                    if (await canLaunch(url)) {
-                      await launch(url, forceSafariVC: false);
-                    } else {
-                      throw 'Could not launch $url';
-                    }
-                  },
-                  icon: const Icon(FontAwesomeIcons.telegramPlane,
-                      color: Colors.white, size: 40),
-                )),
-                ListTile(
-                  leading: IconButton(
-                    onPressed: () async {
-                      const url = 'https://www.mixcloud.com/tysafm/';
-
-                      if (await canLaunch(url)) {
-                        await launch(url, forceSafariVC: false);
-                      } else {
-                        throw 'Could not launch $url';
-                      }
-                    },
-                    icon: const Icon(
-                      FontAwesomeIcons.mixcloud,
-                      color: Colors.white,
-                      size: 40,
-                    ),
-                  ),
-                ),
-                ListTile(
-                  leading: IconButton(
-                    onPressed: () async {
-                      const url = 'http://twitch.tv/tysafm';
-
-                      if (await canLaunch(url)) {
-                        await launch(url, forceSafariVC: false);
-                      } else {
-                        throw 'Could not launch $url';
-                      }
-                    },
-                    icon: const Icon(Icons.connected_tv,
-                        color: Colors.white, size: 40),
-                  ),
-                ),
-                ListTile(
-                  leading: IconButton(
-                    onPressed: () async {
-                      const url = 'tel:+38095 510 2277';
-
-                      if (await canLaunch(url)) {
-                        await launch(url, forceSafariVC: false);
-                      } else {
-                        throw 'Could not launch $url';
-                      }
-                    },
-                    icon: const Icon(Icons.phone_forwarded,
-                        color: Colors.white, size: 40),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
         appBar: AppBar(
           centerTitle: true,
           backgroundColor: const Color(0xff00310b),
@@ -223,6 +91,7 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
             ),
           ),
         ),
+        endDrawer: const EndDrawer(),
         body: Builder(builder: (context) {
           return Container(
             height: MediaQuery.of(context).size.height * 1.0,
@@ -295,3 +164,6 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
     );
   }
 }
+
+
+
